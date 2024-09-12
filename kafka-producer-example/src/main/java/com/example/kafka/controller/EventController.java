8e19a0d6721ea9c20f2aa273ad294ebcd1cb1af8
David Lin <david11yf29@gmail.com>
2024-09-12 13:03:40 -0700

    Finished Obj Coding

--- kafka-producer-example/src/main/java/com/example/kafka/controller/EventController.java
+++ kafka-producer-example/src/main/java/com/example/kafka/controller/EventController.java
@@ -1,13 +1,11 @@
 package com.example.kafka.controller;
 
+import com.example.kafka.dto.Customer;
 import com.example.kafka.service.KafkaMessagePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/producer-app")
@@ -28,5 +26,10 @@ public class EventController {
                     .build();
         }
 
+    }
+
+    @PostMapping("/publish")
+    public void sendEvents(@RequestBody Customer customer) {
+        publisher.sendEventsToTopic(customer);
     }
 }
--- kafka-producer-example/src/main/java/com/example/kafka/service/KafkaMessagePublisher.java
+++ kafka-producer-example/src/main/java/com/example/kafka/service/KafkaMessagePublisher.java
@@ -1,5 +1,6 @@
 package com.example.kafka.service;
 
+import com.example.kafka.dto.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -22,6 +23,22 @@ public class KafkaMessagePublisher {
                 System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
             }
         });
+    }
+
+    public void sendEventsToTopic(Customer customer) {
+        try {
+            CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("topic-obj-1", customer);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    System.out.println("Sent Message=[" + customer.toString() + "] with offset=[" + result.getRecordMetadata().offset() +
+                            "]");
+                } else {
+                    System.out.println("Unable to send message=[" + customer.toString() + "] due to : " + ex.getMessage());
+                }
+            });
+        } catch (Exception e) {
+            System.out.println("ERROR : " + e.getMessage());
+        }
 
     }
 }
